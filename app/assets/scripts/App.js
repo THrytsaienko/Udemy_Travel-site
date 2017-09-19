eb40d0459ce949ee0740b1fbd6753d573722b989
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2017-09-19 22:16:37 +0300

    Modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,15 +2,13 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
-
-
-
-
+var modal = new Modal();
 
 
 
@@ -32,3 +30,5 @@ var stickyHeader = new StickyHeader();
 // var jane = new Adult("Jane Smith", "orange");
 // jane.greet();
 // jane.payTaxes();
+
+// $("h1").remove();
