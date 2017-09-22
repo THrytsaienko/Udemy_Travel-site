c3d943934caf50195dc657b759fa70fb860c699f
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2017-09-22 11:42:46 +0300

    Completed gulp build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -12,13 +12,13 @@ gulp.task('previewDist', function(){
     browserSync.init({
         notify: false,
         server: {
-            baseDir: "dist"
+            baseDir: "docs"
         }
     });
 });
 
-gulp.task('deleteDistFolder', function () {
-    return del("./dist");
+gulp.task('deleteDistFolder', ['icons'], function () {
+    return del("./docs");
 });
 
 gulp.task('copyGeneralFiles', ['deleteDistFolder'], function(){
@@ -32,26 +32,30 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function(){
         '!./app/temp/**'
     ]
     return gulp.src(pathsToCopy)
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./docs"));
 });
 
-gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function () {
+gulp.task('optimizeImages', ['deleteDistFolder'], function () {
     return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
         .pipe(imagemin({
             progressive: true,
             interlaced: true,
             multipass: true
         }))
-        .pipe(gulp.dest("./dist/assets/images"));
+        .pipe(gulp.dest("./docs/assets/images"));
 });
 
-gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function () {
+gulp.task('useminTrigger', ['deleteDistFolder'], function(){
+    gulp.start("usemin");
+});
+
+gulp.task('usemin', ['styles', 'scripts'], function () {
     return gulp.src("./app/index.html")
         .pipe(usemin({
             css: [function(){return rev()}, function(){return cssnano()}],
             js: [function () { return rev() }, function () { return uglify() }]
         }))
-        .pipe(gulp.dest("./dist"));
+        .pipe(gulp.dest("./docs"));
 });
 
-gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
